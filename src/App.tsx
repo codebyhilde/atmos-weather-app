f71d9831f9eff2fce8061996072b92f2bdf355e5
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-11-17 17:18:47 -0400

    feat(ux): implement dedicated Loader and ErrorState components
    - Introduce 'Loader.tsx' component with a spinning animation for visual feedback during loading states
    - Add 'ErrorState.tsx' component to display detailed error messages and provide a 'Retry' action
    - Refactor conditional rendering in 'App.tsx' to utilize the new 'Loader' and 'ErrorState' components
    - Improve clarity in 'App.tsx' by updating 'handleSearch' parameters to reflect the correct type names ('countryCode', 'stateCode')

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header";
+import { Loader } from "./components/Loader.tsx";
+import { ErrorState } from "./components/ErrorState.tsx";
 import { Location } from "./components/Location";
 import { CurrentTime } from "./components/CurrentTime";
 import { WeatherCondition } from "./components/WeatherCondition";
@@ -10,41 +12,65 @@ import { useWeather } from "./hooks/useWeather";
 function App() {
     const { theme, toggleTheme } = useTheme();
     const { data, isLoading, error, fetchWeather } = useWeather();
-    
+
     // Función de búsqueda
-    const handleSearch = (search: { city: string; country: string; state?: string }) => {
+    const handleSearch = (search: {
+        city: string;
+        countryCode: string;
+        stateCode?: string;
+    }) => {
         fetchWeather(search);
     };
 
     // Manejo de estados de carga y error
     if (isLoading) {
-        return <div className="container mx-auto p-4 max-w-lg min-h-screen">Cargando...</div>;
+        return <Loader />;
     }
 
     if (error) {
-        return <div className="container mx-auto p-4 max-w-lg min-h-screen">Error: {error}</div>;
+        return (
+            <div className="container mx-auto p-4 max-w-lg min-h-screen">
+                <Header
+                    theme={theme}
+                    toggleTheme={toggleTheme}
+                    onSearch={handleSearch}
+                />
+
+                <ErrorState
+                    message={error}
+                    onRetry={() => window.location.reload()}
+                />
+            </div>
+        );
     }
 
     // Estado inicial de la App (renderizado cuando no hy datos)
     if (!data) {
         return (
             <div className="container mx-auto p-4 max-w-lg min-h-screen">
-                <Header theme={theme} toggleTheme={toggleTheme} onSearch={handleSearch} />
-                <p className="text-center mt-10">Busca una ciudad para comenzar.</p>
+                <Header
+                    theme={theme}
+                    toggleTheme={toggleTheme}
+                    onSearch={handleSearch}
+                />
+                <p className="text-center mt-10">
+                    Busca una ciudad para comenzar.
+                </p>
             </div>
         );
     }
-    
+
     // Renderizado con datos
     const { timezone, current, hourly, daily } = data;
 
     return (
         <div className="container mx-auto p-4 max-w-lg min-h-screen">
-            <Header theme={theme} toggleTheme={toggleTheme} onSearch={handleSearch} />
-            <Location
-                place={timezone}
-                hour={current.hour}
+            <Header
+                theme={theme}
+                toggleTheme={toggleTheme}
+                onSearch={handleSearch}
             />
+            <Location place={timezone} hour={current.hour} />
             <CurrentTime
                 icon={current.icon}
                 tempCelsius={current.temp}
