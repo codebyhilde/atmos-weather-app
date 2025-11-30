0ed1e9a643ba6b9abd28f695d25dea1f38f1abe9
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-11-30 07:17:41 -0400

    fix(types) add extension to weatherRoutes

--- api/src/routes/weatherRoutes.ts
+++ api/src/routes/weatherRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import { getNormalizedWeather } from "../services/weatherService";
+import { getNormalizedWeather } from "../services/weatherService.js";
 
 const router = Router();
 
